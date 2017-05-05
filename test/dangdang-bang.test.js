981dc7910fa5df2c7b4436ab0cd4ad34ff75dd09
arivin <btc022003@gmail.com>
2017-05-05 22:46:07 +0800

    完成当当网热销榜数据的提取

---
+++ test/dangdang-bang.test.js
@@ -0,0 +1,54 @@
+var qdDDBang = require('../index').DangDang.Bang
+const expect = require('chai').expect
+const Table = require('easy-table')//用于打印输出结果
+
+describe('测试当当网相关数据提取', () => {
+    it('热销榜数据长度大于1', (done) => {
+        qdDDBang.getBestSellers((res) => {
+            expect(res.length).to.be.above(1)
+            console.log(Table.print(res))
+            done()
+        })
+    })
+    it('热销榜获取数据时,传递一个超级大的页数', () => {
+        qdDDBang.getBestSellers((res) => {
+            expect(res.length).to.be.above(1)
+            console.log(Table.print(res))
+            done()
+        })
+    }, 10000)
+    it('热销榜总页数必须为数字', done => {
+        qdDDBang.getBestSellersPageCount(res => {
+            expect(res).to.be.a.number
+            done()
+        })
+    })
+    it('热销榜总页数必须为大与1', done => {
+        qdDDBang.getBestSellersPageCount(res => {
+            expect(res).to.be.above(1)
+            done()
+        })
+    })
+    // it('测试获取所有的热销榜数据', done => {
+    //     var allData = []
+    //     qdDDBang.getBestSellersPageCount(pageCount => {
+    //         var pageIndex = 1
+    //         function getTemData(){
+    //             if(pageIndex<=pageCount){
+    //                 qdDDBang.getBestSellers(data => {
+    //                     allData = allData.concat(data)
+    //                     if (pageIndex == pageCount) {
+    //                         expect(allData.length).to.be.above(1)
+    //                         console.log(Table.print(allData))
+    //                         done()
+    //                     }
+    //                     console.log(`第${pageIndex}页数据提取成功`)
+    //                     pageIndex += 1
+    //                     getTemData()
+    //                 }, pageIndex)
+    //             }
+    //         }
+    //         getTemData()
+    //     })
+    // })
+})
